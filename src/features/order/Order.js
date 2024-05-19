500b7930a8609f37afd6d586e17d1d775c05ddfc
Dhananjay Yadav <dhananjay16642@gmail.com>
2024-05-19 20:56:31 +0530

    my first project commit agromart

---
+++ src/features/order/Order.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {  } from './orderSlice';
+
+export default function Order() {
+  const dispatch = useDispatch();
+
+
+  return (
+    <div>
+      <div>
+
+
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/ContactPage.js
@@ -0,0 +1,13 @@
+import NavBar from '../features/navbar/Navbar';
+import Contact from '../features/contact/Contact';
+function ContactPage() {
+    return (
+        <div>
+          <NavBar>
+            <Contact></Contact>
+          </NavBar>
+        </div>
+      );
+}
+
+export default ContactPage;
